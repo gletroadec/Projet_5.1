31fe5617a19fe4cbea8f188a3f64b210a52bebc3
Gaël Le Troadec <gael.le-troadec@etu.univ-nantes.fr>
2021-12-02 20:29:32 +0100

    ajout des modifs d'antoine

--- Scripts/6_Dictionary.py
+++ Scripts/6_Dictionary.py
@@ -1,10 +1,20 @@
-Testlist=list(Test_graph23.edges)
-Testlist2=list(Testlist)
+#Création d'un dictionnaire associant nom du noeud au nom du modèle 
+model_dictionary = {}
+for i in range (len(Pattern)) :
+  for node in Test_graph23.nodes : 
+    if Pattern[i] in node :
+      model_dictionary[node]=pattern[i]
+
+#Ce code créé une liste parallèle contenant les couple et le noms de leur modèles
+TTest=[]
+Testlist = list(Test_graph23.edges)
 for i in range(len(Testlist)):
-  for e in range(1):
-    Testlist2=list(Testlist2)
-    x=Testlist2[i][e]
-    x=model_dictionary[x]
-    list(x)
-    Testlist2[i][e]=list(x)
-    
+  oui=0
+  oui=[]
+  #oui=list(Testlist[i])
+  oui=Testlist[i]
+  oui=list(oui)
+  for e in range(len(oui)):
+    oui[e]=model_dictionary[oui[e]]
+    #oui=list(oui)
+    Testlist[i]=oui
